8bfa14895f99dafed16d8f2fdf68eb74ecf2c937
Caique Queiroz <caiqueonisto@live.com>
2023-05-02 23:15:58 -0300

    finalizado 50 %

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,34 @@
 import { Component } from '@angular/core';
+import { Todo } from 'src/models/todo.model';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'todo';
+export class AppComponent { 
+  public todos: Todo[] = [];
+  public title: String = 'Minhas Tarefas';
+
+  constructor() {
+    this.todos.push(new Todo(1, 'Brincar com Leo', false));
+    this.todos.push(new Todo(2, 'Brincar com a Antonela', false));
+    this.todos.push(new Todo(3, 'Brincar com os dois', false));
+  }
+
+  remove(todo: Todo) {
+    const index = this.todos.indexOf(todo);
+
+    if (index != -1){
+      this.todos.splice(index, 1);
+    }
+  }
+
+  taskDone(todo: Todo) {
+    todo.done = true;
+  }
+
+  taskUnDone(todo: Todo) {
+    todo.done = false;
+  }
 }
